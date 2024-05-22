2fb185a006c598bc2e1d56fbcd48ab80f49ec5b4
anas <anas@ensemble.com>
2024-05-22 16:20:20 +0100

    Table - countries

--- blocks/table/table.js
+++ blocks/table/table.js
@@ -1,39 +1,56 @@
 import { fetchPlaceholders, getMetadata } from '../../scripts/aem.js';
 const placeholders = await fetchPlaceholders(getMetadata('locale'));
 
-const { africa, america, asia, australia, europe, sNo } = placeholders;
+const {
+  allCountries,
+  abbreviation,
+  africa,
+  america,
+  asia,
+  australia,
+  capital,
+  continent,
+  countries,
+  europe,
+  sNo,
+} = placeholders;
 
 async function createTableHeader(table) {
   let tr = document.createElement('tr');
   let sno = document.createElement('th');
-  sno.appendChild(document.createTextNode(sno));
-  let section = document.createElement('th');
-  section.appendChild(document.createTextNode(section));
-  let question = document.createElement('th');
-  question.appendChild(document.createTextNode(question));
-  let answer = document.createElement('th');
-  answer.appendChild(document.createTextNode(answer));
+  sno.appendChild(document.createTextNode('sNo'));
+  let conuntry = document.createElement('th');
+  conuntry.appendChild(document.createTextNode('countries'));
+  let continenth = document.createElement('th');
+  continenth.appendChild(document.createTextNode('continent'));
+  let capitalh = document.createElement('th');
+  capitalh.appendChild(document.createTextNode('capital'));
+  let abbr = document.createElement('th');
+  abbr.appendChild(document.createTextNode('abbreviation'));
   tr.append(sno);
-  tr.append(section);
-  tr.append(question);
-  tr.append(answer);
+  tr.append(conuntry);
+  tr.append(capitalh);
+  tr.append(continenth);
+  tr.append(abbr);
   table.append(tr);
 }
-
 async function createTableRow(table, row, i) {
   let tr = document.createElement('tr');
   let sno = document.createElement('td');
   sno.appendChild(document.createTextNode(i));
-  let section = document.createElement('td');
-  section.appendChild(document.createTextNode(row.Section));
-  let question = document.createElement('td');
-  question.appendChild(document.createTextNode(row.Question));
-  let answer = document.createElement('td');
-  answer.appendChild(document.createTextNode(row.Answer));
+  let country = document.createElement('td');
+  country.appendChild(document.createTextNode(row.Country));
+  let continent = document.createElement('td');
+  continent.appendChild(document.createTextNode(row.Capital));
+  let capital = document.createElement('td');
+  capital.appendChild(document.createTextNode(row.Continent));
+  let abbr = document.createElement('td');
+  abbr.appendChild(document.createTextNode(row.Abbreviation));
   tr.append(sno);
-  tr.append(section);
-  tr.append(question);
-  tr.append(answer);
+  tr.append(country);
+  tr.append(continent);
+  tr.append(capital);
+  tr.append(abbr);
   table.append(tr);
 }
 
@@ -42,15 +59,15 @@ async function createSelectMap(jsonURL) {
   const { pathname } = new URL(jsonURL);
 
   const resp = await fetch(pathname);
-  optionsMap.set('all', allQuestions);
-  optionsMap.set('asia', asia);
-  optionsMap.set('europe', europe);
-  optionsMap.set('africa', africa);
-  optionsMap.set('america', america);
-  optionsMap.set('australia', australia);
+  optionsMap.set('all', 'allCountries');
+  optionsMap.set('asia', 'asia');
+  optionsMap.set('europe', 'europe');
+  optionsMap.set('africa', 'africa');
+  optionsMap.set('america', 'america');
+  optionsMap.set('australia', 'australia');
   const select = document.createElement('select');
-  select.id = 'section';
-  select.name = 'section';
+  select.id = 'region';
+  select.name = 'region';
   optionsMap.forEach((val, key) => {
     const option = document.createElement('option');
     option.textContent = val;
@@ -59,28 +76,21 @@ async function createSelectMap(jsonURL) {
   });
 
   const div = document.createElement('div');
-  div.classList.add('section-select');
+  div.classList.add('region-select');
   div.append(select);
   return div;
 }
-
 async function createTable(jsonURL, val) {
-  let pathName = null;
-
-  console.log('jsonURL: ', jsonURL);
-  console.log('val: ', val);
-
+  let pathname = null;
   if (val) {
-    pathName = jsonURL;
+    pathname = jsonURL;
   } else {
-    pathName = new URL(jsonURL);
+    pathname = new URL(jsonURL);
   }
 
-  console.log('pathName: ', pathName);
-
-  const resp = await fetch(pathName);
-  const json = resp.json;
-  console.log('JSON: {}', json);
+  const resp = await fetch(pathname);
+  const json = await resp.json();
+  console.log('=====JSON=====> {} ', json);
 
   const table = document.createElement('table');
   createTableHeader(table);
@@ -92,13 +102,25 @@ async function createTable(jsonURL, val) {
 }
 
 export default async function decorate(block) {
-  const faq = block.querySelector('p');
-  console.log(faq);
-  const parentDiv = document.createElement('div');
-  parentDiv.classList.add('faq-block');
+  const countries = block.querySelector('a[href$=".json"]');
+  const parientDiv = document.createElement('div');
+  parientDiv.classList.add('contries-block');
 
-  if (faq) {
-    parentDiv.append(await createTable(faq.innerText, 'helix-default'));
-    faq.replaceWith(parentDiv);
+  if (countries) {
+    parientDiv.append(await createSelectMap(countries.href));
+    parientDiv.append(await createTable(countries.href, null));
+    countries.replaceWith(parientDiv);
   }
+  const dropdown = document.getElementById('region');
+  dropdown.addEventListener('change', () => {
+    let url = countries.href;
+    if (dropdown.value != 'all') {
+      url = countries.href + '?sheet=' + dropdown.value;
+    }
+    const tableE = parientDiv.querySelector(':scope > table');
+    let promise = Promise.resolve(createTable(url, dropdown.value));
+    promise.then(function (val) {
+      tableE.replaceWith(val);
+    });
+  });
 }
